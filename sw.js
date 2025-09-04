d0599bcf0ea3cb3f2f3b838aae6f8ae0a8c00da5
IvyKe <159719404+IvyKanana@users.noreply.github.com>
2025-09-04 16:16:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('polywaste-v2').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './app.js',
-      './manifest.webmanifest',
-      './assets/icon-192.png',
-      './assets/icon-512.png',
-      'https://cdn.jsdelivr.net/npm/html2pdf.js@0.10.1/dist/html2pdf.bundle.min.js',
-      'https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js'
-    ]))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("polywaste-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.webmanifest"
+      ]);
+    })
   );
-  self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!=='polywaste-v2').map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
